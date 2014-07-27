c73dfff58a71b8ff46d6215f8330dc15dcdc5e4f
Bryant Luk <bryant.luk@bryantluk.com>
2014-07-27 15:40:04 -0500

    Initial Commit

---
+++ Torrent/Torrent.h
@@ -0,0 +1,19 @@
+//
+//  Torrent.h
+//  Torrent
+//
+//  Created by Bryant Luk on 7/27/14.
+//  Copyright (c) 2014 OOB. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Torrent.
+FOUNDATION_EXPORT double TorrentVersionNumber;
+
+//! Project version string for Torrent.
+FOUNDATION_EXPORT const unsigned char TorrentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Torrent/PublicHeader.h>
+
+
